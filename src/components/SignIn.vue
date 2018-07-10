48f69d09be4c3884a23d0c4ec732f30b31f25e52
jordanhudgens <jordan@devcamp.com>
2018-07-10 15:15:39 -0600

    Added skeleton sign in and registration components

---
+++ src/components/SignIn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Sign In
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignIn />
+    <Register />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SignIn from '@/components/SignIn.vue'
+import Register from '@/components/Register.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    SignIn,
+    Register
   }
 }
 </script>
