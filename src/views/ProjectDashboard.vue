0f8866b87a047299abc49cd53bf0d29b45a1e470
jordanhudgens <jordan@devcamp.com>
2018-07-12 16:51:32 -0600

    Added demo objects

--- src/views/ProjectDashboard.vue
+++ src/views/ProjectDashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     Project Dashboard
+
+    <div v-for="project in projects" :key="project.id">
+      <div>
+        {{ project.title }}
+      </div>
+
+      <div>
+        {{ project.mainObjective }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +19,23 @@ export default {
   name: 'ProjectDashboard',
   data() {
     return {
+      projects: [
+        {
+          id: 1,
+          title: 'First Project',
+          mainObjective: 'Content for the main objective'
+        },
+        {
+          id: 2,
+          title: 'Second Project',
+          mainObjective: 'Content for the main objective for project 2'
+        },
+        {
+          id: 3,
+          title: 'Third Project',
+          mainObjective: 'Content for the main objective for project 3 and testing out a little longer main objective to see what that looks like'
+        }
+      ]
     }
   }
 }
