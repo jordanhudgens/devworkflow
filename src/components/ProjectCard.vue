7e8ee4e343d92969f2bf0d874ab605fd52a00a5e
jordanhudgens <jordan@devcamp.com>
2018-10-11 14:27:41 -0500

    Converted the html for project cards into an abstract component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="metadata">
+      <div class='title'>
+        {{ project.title }}
+      </div>
+
+      <div class="main-objective">
+        {{ project.main_objective }}
+      </div>
+    </div>
+
+    <div class="line-items">
+
+      <div class="line-item">
+        <span class="completed-circle"></span>
+        <span class="title">
+          Wireframes
+        </span>
+      </div>
+
+      <div class="line-item">
+        <span class="incompleted-circle"></span>
+        <span class="title">
+          Sitemap
+        </span>
+      </div>
+
+      <div class="line-item">User Stories</div>
+      <div class="line-item">UML Diagrams</div>
+      <div class="line-item">Hi-Res Designs</div>
+      <div class="line-item">Logo/Images</div>
+      <div class="line-item">Frontend Implementation</div>
+      <div class="line-item">Backend API</div>
+      <div class="line-item">Automated Tests</div>
+      <div class="line-item">Deployment</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectCard',
+
+  data() {
+    return {
+    }
+  },
+
+  props: {
+    project: Object
+  }
+}
+</script>
--- src/views/ProjectDashboard.vue
+++ src/views/ProjectDashboard.vue
@@ -14,41 +14,7 @@
       </div>
 
       <div v-for="project in projects" :key="project.id" class="project-card">
-        <div class="metadata">
-          <div class='title'>
-            {{ project.title }}
-          </div>
-
-          <div class="main-objective">
-            {{ project.main_objective }}
-          </div>
-        </div>
-
-        <div class="line-items">
-
-          <div class="line-item">
-            <span class="completed-circle"></span>
-            <span class="title">
-              Wireframes
-            </span>
-          </div>
-
-          <div class="line-item">
-            <span class="incompleted-circle"></span>
-            <span class="title">
-              Sitemap
-            </span>
-          </div>
-
-          <div class="line-item">User Stories</div>
-          <div class="line-item">UML Diagrams</div>
-          <div class="line-item">Hi-Res Designs</div>
-          <div class="line-item">Logo/Images</div>
-          <div class="line-item">Frontend Implementation</div>
-          <div class="line-item">Backend API</div>
-          <div class="line-item">Automated Tests</div>
-          <div class="line-item">Deployment</div>
-        </div>
+        <ProjectCard :project="project" />
       </div>
     </div>
   </div>
@@ -58,6 +24,7 @@
 import axios from 'axios';
 import NewProject from '@/components/NewProject';
 import ProjectDashboardNav from '@/components/ProjectDashboardNav';
+import ProjectCard from '@/components/ProjectCard';
 
 export default {
   name: 'ProjectDashboard',
@@ -76,7 +43,8 @@ export default {
 
   components: {
     NewProject,
-    ProjectDashboardNav
+    ProjectDashboardNav,
+    ProjectCard
   },
 
   methods: {
