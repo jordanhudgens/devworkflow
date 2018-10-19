34be72717fb9a7280445406624ad5bbcd7a1446d
jordanhudgens <jordan@devcamp.com>
2018-10-19 17:00:48 -0500

    Updated the vuex store with live values

--- src/store.js
+++ src/store.js
@@ -5,47 +5,14 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    selectedProjectItemTitle: null,
-    globalTitle: "My Custom Title",
-    links: ["http://google.com", "http://coursetro.com", "http://youtube.com"]
-  },
-
-  getters: {
-    countLinks: state => {
-      return state.links.length;
-    }
+    selectedProjectItemTitle: null
   },
 
   mutations: {
     SET_SELECTED_PROJECT_ITEM_TITLE: (state, projectItemTitle) => {
       state.selectedProjectItemTitle = projectItemTitle;
-    },
-
-    ADD_LINK: (state, link) => {
-      state.links.push(link);
-    },
-
-    REMOVE_LINK: (state, link) => {
-      state.links.splice(link, 1);
-    },
-
-    REMOVE_ALL: state => {
-      state.links = [];
     }
   },
 
-  actions: {
-    removeLink: (context, link) => {
-      context.commit("REMOVE_LINK", link);
-    },
-
-    removeAll({ commit }) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          commit("REMOVE_ALL");
-          resolve();
-        }, 1500);
-      });
-    }
-  }
+  actions: {}
 });
